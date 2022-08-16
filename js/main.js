d5ddabba98e028615041b505bd38c817965f6a52
Ilya <ilya228medvedev1337@gmail.com>
2022-08-16 23:43:28 +0300

    New Path

--- js/main.js
+++ js/main.js
@@ -23,8 +23,8 @@ if (iconMenu) {
    const menuBody = document.querySelector('.menu__body')
    iconMenu.addEventListener("click", function (e) {
       document.body.classList.toggle('_lock');
-      iconMenu.classList.toggle('_active');
       menuBody.classList.toggle('_active');
+      iconMenu.classList.toggle('_active');
       iconMenu.classList.toggle('_menu-open');
       })
 }
