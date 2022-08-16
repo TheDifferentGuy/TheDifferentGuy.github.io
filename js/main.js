33fd33dd760228696ce9233756ef62a0e17394ba
Ilya <ilya228medvedev1337@gmail.com>
2022-08-17 00:14:37 +0300

    New Commit

--- js/main.js
+++ js/main.js
@@ -1,26 +1,6 @@
-let menulinks = document.querySelectorAll('.menu__link[data-goto]')
-if (menulinks.length > 0) {
-   menulinks.forEach(menulinks => {
-      menulinks.addEventListener("click",onMenuLinkClick)
-   });
-
-   function onMenuLinkClick(e) {
-      const menuLink = e.target;
-      if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-         const gotoBlock = document.querySelector(menuLink.dataset.goto);
-         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-         
-         window.scrollTo({
-            top: gotoBlockValue,
-            behavior: "smooth"
-         });
-      }
-   }
-}
-
 const iconMenu = document.querySelector('.icon-menu');
+const menuBody = document.querySelector('.menu__body')
 if (iconMenu) {
-   const menuBody = document.querySelector('.menu__body')
    iconMenu.addEventListener("click", function (e) {
       document.body.classList.toggle('_lock');
       menuBody.classList.toggle('_active');
@@ -29,5 +9,29 @@ if (iconMenu) {
       })
 }
 
+let menulinks = document.querySelectorAll('.menu__link[data-goto]')
+if (menulinks.length > 0) {
+   menulinks.forEach(menulinks => {
+      menulinks.addEventListener("click", onMenuLinkClick)
+   });
 
+   function onMenuLinkClick(e) {
+      const menuLink = e.target;
+      if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+         const gotoBlock = document.querySelector(menuLink.dataset.goto);
+         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
 
+         if (iconMenu.classList.contains('_active')) {
+            document.body.classList.remove('_lock');
+            menuBody.classList.remove('_active');
+            iconMenu.classList.remove('_active');
+            iconMenu.classList.remove('_menu-open');
+         }
+
+         window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+         });
+      }
+   }
+}
