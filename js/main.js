67397c22fc86d4707e48477dc0d67c1e6215204d
Ilya <ilya228medvedev1337@gmail.com>
2022-08-16 20:54:21 +0300

    New Update

---
+++ js/main.js
@@ -0,0 +1,33 @@
+let menulinks = document.querySelectorAll('.menu__link[data-goto]')
+if (menulinks.length > 0) {
+   menulinks.forEach(menulinks => {
+      menulinks.addEventListener("click",onMenuLinkClick)
+   });
+
+   function onMenuLinkClick(e) {
+      const menuLink = e.target;
+      if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+         const gotoBlock = document.querySelector(menuLink.dataset.goto);
+         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+         
+         window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+         });
+      }
+   }
+}
+
+const iconMenu = document.querySelector('.icon-menu');
+if (iconMenu) {
+   const menuBody = document.querySelector('.menu__body')
+   iconMenu.addEventListener("click", function (e) {
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+      iconMenu.classList.toggle('_menu-open');
+      })
+}
+
+
+
